becee6ab2e6c065ed87b3bb355eb9f13531e56bf
Ryan Woodworth <rswoodworth29@gmail.com>
2020-03-29 16:47:59 -0400

    Part A.1 done, nonface images cropped, now need to split all training into training and validation sets

--- code_and_images/generate_cropped_notfaces.m
+++ code_and_images/generate_cropped_notfaces.m
@@ -12,7 +12,28 @@ mkdir(new_imageDir);
 dim = 36;
 
 while n_have < n_want
-    
     % generate random 36x36 crops from the non-face images
     
+    %Get random image from not_faces
+    ind = randi([1 nImages], 1, 1);
+    tempImg = imread(append(imageList(ind).folder, '/', imageList(ind).name));
+    tempImg = im2double(rgb2gray(tempImg));
+    
+    %Make temp 36x36 image
+    tempCrop = zeros(dim,dim);
+    
+    %Get 36x36 cropped image from tempImg
+    [row, col, ~] = size(tempImg);
+    randOrigRow = randi([1 (row - dim)], 1,1);
+    randOrigCol = randi([1 (col - dim)], 1,1);
+    
+    tempCrop(1:36,1:36) = tempImg(randOrigRow:randOrigRow + 35, randOrigCol:randOrigCol + 35);
+    path = append(new_imageDir,'/',num2str(n_have),'.jpg');
+    imwrite(tempCrop,path);
+    
+    %Increase loop
+    n_have = n_have + 1;
+    
 end
+
+
